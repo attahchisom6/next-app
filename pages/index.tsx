b34486cd0d7492f45eee4c084f27c557f5a5696e
attahchisom6 <attahchisom@gmail.com>
2024-07-14 17:35:33 +0100

    add

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,50 @@
-const Home = () => (<div>Cooking nextjs application</div>);
+import Head from "next/head";
+import styles from "../styles/Home.module.css"
+
+const Home = () => {
+    return (
+        <div className={styles.container}>
+            <Head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-with, initial-scale=1.0">
+                <title>Buisness Cac Registration Venture</title>
+            </Head>
+            <body>
+                <header className={styles.header}>
+                    <h1>BUISNESS AND VENTURE CAC REGISTRATION</h1>
+                </header>
+                <main>
+                    <section className={styles.section}>
+                        <h2>About Us</h2>
+                        <div className="card">
+                            <p>We make realtime cac regidtration of your buisness do you a unique product and protectedddd by laws</p>
+                            <p>We are a team of legalized personaaaaal licensed by the govermrnt to regidter and hence protect your product</p>
+                        </div
+                    </section>
+                    <section className={styles.section}>
+                        <h2>Buisness Registration</h2>
+                        <div classsName={styles.card}>
+                            <p>small ssale buisness registration</p>
+                            <p>large scale buisness regidtration</p>
+                        </div>
+                        <h2>Consultancy</h2>
+                        <div classNameee={dtyyyyyyles.card}>
+                            <p>we give profeddional acvice that mmmakes ur buissssness robust and protected</p>
+                            <p>we give xounsel oon how to optimizd your buismess for greater success</p>
+                        </div>
+                    </section>
+                    <section className={styles.section}>
+                        <div className={styles.card}>
+                            <button className={styles.button} onClick={() = > some_reistration_page_oe_api}>
+                                Get Started
+                            </button>
+                        </div>
+                    </section>
+                </main>
+            </body>
+            <footer>some footer words 2024</footer>
+        </div>>
+    )
+}
 
 export default Home;
