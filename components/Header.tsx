e9a193bffd6c9c07ea975be346749b7e89cd46d3
attahchisom6 <attahchisom@gmail.com>
2024-07-21 23:37:53 +0100

    add

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,26 +1,31 @@
-import  headerStyle from "../styles/HeaderStyle.module.css";
 
-interface headerProps {
+import React from "react";
+import headerStyle from "../styles/HeaderStyle.module.css";
+import styles from "../styles/globals.module.css";
+import navStyle from "../styles/NavStyles.module.css";
+import NavBar from "./NavBar"; // Import NavBar component
+
+interface HeaderProps {
     theme: string;
 }
 
-const ids = ["hero", "services", "works", "about",  "vlog", "testimonials", "contact"];
+const ids = ["hero", "services", "works", "about", "vlog", "testimonials", "contact"];
 
-const Header = ({theme}: headerProps) => {
-  return (
-    <div className="container">
-      <header className={headerStyle.header} data-section-theme={theme}>
-        <div class={headerStyle.header-logo}>
-          <img src="../public/favour-cac-logo.jpg">
+const Header = ({ theme }: HeaderProps) => {
+    return (
+        <div className={styles.container}>
+            <header className={headerStyle.header} data-section-theme={theme}>
+                <div className={headerStyle["header-logo"]}>
+                    <img src="/favour-cac-logo.jpg" alt="favours-org" />
+                </div>
+                <input className={headerStyle["menu-button"]} id="menu-btn" type="checkbox" />
+                <label className={headerStyle["menu-icon"]} htmlFor="menu-btn">
+                    <span className={navStyle.navicon}></span>
+                </label>
+                <NavBar list={ids} />
+            </header>
         </div>
-        <input className={headerStyle.menu-button} id={menu-btn}>
-        <label className={headerStyle.menu-icon} htmlFor={menu-btn}>
-          <span className={headerStyle.navicon></span>
-          </label>
-          <NavBar list={ids} />
-      </header>
-    </div>
-  );
+    );
 }
 
 export default Header;
